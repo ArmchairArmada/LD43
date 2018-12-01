218ddacfd29e6973e5e86093144eb7c3110a8037
Nate <Nate>
2018-12-01 13:42:58 -0500

    Asset management and sprites

---
+++ src/js/sprites.js
@@ -0,0 +1,34 @@
+function SpriteManager() {
+    this.sprites = [];
+}
+
+SpriteManager.prototype.draw = function(ctx, camera) {
+    this.sprites.sort(function(a, b) {
+        return b.z - a.z;
+    });
+
+    for (var i=0; i<this.sprites.length; i++) {
+        this.sprites[i].draw(ctx, camera);
+    }
+};
+
+SpriteManager.prototype.add = function(sprite) {
+    this.sprites.push(sprite);
+};
+
+SpriteManager.prototype.remove = function(sprite) {
+    this.sprites.remove(sprite);
+};
+
+
+// --------------------------------------------------------------------------------------------------------------------
+function Sprite() {
+    this.x = 0;
+    this.y = 0;
+    this.z = 0;
+    this.image = null;
+}
+
+Sprite.prototype.draw = function(ctx, camera) {
+    ctx.drawImage(this.image, this.x - camera.x, this.y - camera.y);
+}
