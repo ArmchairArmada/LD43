7b6762c3c61fe51e5927c8c43cf87f743533a55e
Nate <Nate>
2018-12-02 21:01:43 -0500

    Added basic character control

--- src/js/entities.js
+++ src/js/entities.js
@@ -22,7 +22,7 @@ EntityManager.prototype.add = function(entity) {
 };
 
 EntityManager.prototype.remove = function(entity) {
-    this.entities.remove(entity);
+    removeFromArray(this.entities, entity);
 };
 
 EntityManager.prototype.killAll = function() {
@@ -61,7 +61,7 @@ TestEntity.prototype.update = function() {
     this.sprite.move(
         this.sprite.rect.x + this.dx,
         this.sprite.rect.y + this.dy);
-    this.sprite.z = -this.sprite.rect.y;
+    this.sprite.z = -this.sprite.rect.y - this.sprite.rect.height;
 
     return true;
 };
@@ -69,4 +69,99 @@ TestEntity.prototype.update = function() {
 TestEntity.prototype.kill = function() {
     console.log("TestEntity killed");
     this.scene.spriteManager.remove(this.sprite);
+    this.scene.entityManager.remove(this);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+const PLAYER_ACCEL = 0.5;
+const PLAYER_DRAG = 0.13;
+
+function PlayerEntity(scene) {
+    this.scene = scene;
+
+    this.sprite = new Sprite(
+        scene.spriteManager,
+        services.assetManager.images["wizard.png"],
+        400,
+        300,
+        0);
+
+    this.vx = 0;
+    this.vy = 0;
+}
+
+PlayerEntity.prototype.update = function() {
+    var ix = 0;
+    var iy = 0;
+
+    if (services.inputManager.keyUp) {
+        iy -= 1;
+    }
+
+    if (services.inputManager.keyDown) {
+        iy += 1;
+    }
+
+    if (services.inputManager.keyLeft) {
+        ix -= 1;
+    }
+
+    if (services.inputManager.keyRight) {
+        ix += 1;
+    }
+
+    var d = Math.sqrt(ix * ix + iy * iy);
+    if (d > 0) {
+        ix = ix / d;
+        iy = iy / d;
+    }
+
+    this.vx += PLAYER_ACCEL * ix;
+    this.vy += PLAYER_ACCEL * iy;
+
+    this.vx -= this.vx * PLAYER_DRAG;
+    this.vy -= this.vy * PLAYER_DRAG;
+
+    this.sprite.move(
+        this.sprite.rect.x + this.vx,
+        this.sprite.rect.y + this.vy);
+
+    this.sprite.z = -this.sprite.rect.y - this.sprite.rect.height;
+
+    this.scene.camera.x = this.sprite.rect.x - globals.canvasWidth/2 + 25;
+    this.scene.camera.y = this.sprite.rect.y - globals.canvasHeight/2 + 25;
+
+    return true;
+};
+
+PlayerEntity.prototype.kill = function() {
+    this.scene.spriteManager.remove(this.sprite);
+    this.scene.entityManager.remove(this);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+function TargetEntity(scene) {
+    this.scene = scene;
+
+    this.sprite = new Sprite(
+        scene.spriteManager,
+        services.assetManager.images["target.png"],
+        400,
+        300,
+        -10000);
+}
+
+TargetEntity.prototype.update = function() {
+    if (services.inputManager.mouseButton && services.inputManager.mouseButtonChange) {
+        console.log("Click: " + services.inputManager.mouseX + ", " + services.inputManager.mouseY);
+    }
+
+    this.sprite.move(this.scene.camera.x + services.inputManager.mouseX-25, this.scene.camera.y + services.inputManager.mouseY-25);
+
+    return true;
+}
+
+TargetEntity.prototype.kill = function() {
+    this.scene.spriteManager.remove(this.sprite);
+    this.scene.entityManager.remove(this);
 };
--- src/js/tilemap.js
+++ src/js/tilemap.js
@@ -1,5 +1,5 @@
-function TileMap(tilesGfx, tileWidth, tileHeight, tileData) {
-    this.tilesGfx = tilesGfx;
+function TileMap(tiles, tileWidth, tileHeight, tileData) {
+    this.tiles = tiles;
     this.tileWidth = tileWidth;
     this.tileHeight = tileHeight;
     this.tileData = tileData;
@@ -7,24 +7,16 @@ function TileMap(tilesGfx, tileWidth, tileHeight, tileData) {
     this.spriteTiles = [];
 }
 
-TileMap.prototype.draw = function(ctx, camera) {
-    // TODO: Smarter drawing to avoid drawing off screen
-    for (var y=0; y<this.tileData.length; y++) {
-        for (var x=0; x<this.tileData[y].length; x++) {
-            ctx.drawImage(this.tilesGfx[this.tileData[y][x]], x * this.tileWidth - camera.x, y * this.tileHeight - camera.y);
-        }
-    }
-};
-
 TileMap.prototype.addSpriteTiles = function(spriteManager) {
     for (var y=0; y<this.tileData.length; y++) {
         for (var x=0; x<this.tileData[y].length; x++) {
+            var tile = this.tiles[this.tileData[y][x]];
             var sprite = new Sprite(
                 spriteManager,
-                this.tilesGfx[this.tileData[y][x]],
+                tile.image,
                 x * this.tileWidth,
                 y * this.tileHeight,
-                this.tileHeight - y * this.tileHeight);
+                -this.tileHeight - y * this.tileHeight + tile.depth);
             this.spriteTiles.push(sprite);
         }
     }
